25bfb96571be85e1569c9598115dc59b38f22457
OnurKader <onurorkunkader1999@gmail.com>
2019-04-04 02:13:08 +0300

    Completed client.py

--- client.py
+++ client.py
@@ -2,11 +2,13 @@ import socket
 import sys
 import errno
 
+
 HEADER_LEN = 10
 IP = "127.0.0.1"
 PORT = 4269
 
 username = input("Please Enter a Username: ")
+uname = username
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
     client_socket.connect((IP, PORT))
@@ -19,6 +21,28 @@ username_header = f"{len(username_encoded):<{HEADER_LEN}}".encode("utf-8")
 client_socket.send(username_header + username_encoded)
 
 while True:
-    pass
+    username = uname
+    message = input(f"{username}> ")
+    if message:
+        message = message.encode("utf-8")
+        message_header = f"{len(message):<{HEADER_LEN}}".encode("utf-8")
+        client_socket.send(message_header + message)
 
+    try:
+        while True:
+            username_header = client_socket.recv(HEADER_LEN)
+            if not len(username_header):
+                print("Connection Terminated by the Server")
+                sys.exit()
+            username_length = int(username_header.decode("utf-8").strip())
+            username = client_socket.recv(username_length).decode("utf-8")
+            print(f"{username}> {message}")
+    except IOError as e:
+        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+            print("Message Could not be Read", str(e))
+            sys.exit()
+        continue
+    except Exception as e:
+        print("Caught an Error!", str(e))
+        sys.exit()
 
