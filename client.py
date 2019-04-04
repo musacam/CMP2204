feda4129a60a6fdf305b9b46e046fc4d60c3a9da
OnurKader <onurorkunkader1999@gmail.com>
2019-04-04 03:35:33 +0300

    Fixed the broken part Forgot to receive message ?!?!?!

--- client.py
+++ client.py
@@ -8,7 +8,7 @@ IP = "127.0.0.1"
 PORT = 4269
 
 username = input("Please Enter a Username: ")
-uname = username
+uname = username.strip(" ").strip("\n")
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
     client_socket.connect((IP, PORT))
@@ -16,25 +16,31 @@ except:
     PORT += 1
     client_socket.connect((IP, PORT))
 client_socket.setblocking(False)
-username_encoded = username.encode("utf-8")
-username_header = f"{len(username):<{HEADER_LEN}}".encode("utf-8")
+username_encoded = username.encode()
+username_header = f"{len(username_encoded):<{HEADER_LEN}}".encode()
 client_socket.send(username_header + username_encoded)
 
 while True:
-    message = input(f"{username}> ").strip("\n")
+    username = uname
+    message = input(f"{username}> ")
     if message:
-        message = message.encode("utf-8")
-        message_header = f"{len(message):<{HEADER_LEN}}".encode("utf-8")
+        message = message.encode()
+        message_header = f"{len(message):<{HEADER_LEN}}".encode()
         client_socket.send(message_header + message)
     try:
         while True:
-            temp_header = client_socket.recv(HEADER_LEN)
-            if not len(temp_header):
+            username_header = client_socket.recv(HEADER_LEN)
+            if not len(username_header):
                 print("Connection Terminated by the Server")
                 sys.exit()
-            temp_length = int(temp_header.decode("utf-8"))
-            temp = client_socket.recv(temp_length).decode("utf-8")
-            print(f"{temp}> {message}")
+            username_length = int(username_header.decode().strip())
+            username = client_socket.recv(username_length).decode()
+
+            message_header = client_socket.recv(HEADER_LEN)
+            message_length = int(message_header.decode().strip())
+            message = client_socket.recv(message_length).decode()
+
+            print(f"\n{username}> {message}\n")
     except IOError as e:
         if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
             print("Message Could not be Read", str(e))
