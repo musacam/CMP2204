6e8599f47ffbcf03a524e46d7fc01440c8bb4c57
OnurKader <onurorkunkader1999@gmail.com>
2019-04-04 21:54:55 +0300

    Modified server.py to output to a file with timestamps message_log.txt Also in .gitignore

--- server.py
+++ server.py
@@ -1,7 +1,17 @@
 import socket
 import select
-import datetime
+from datetime import datetime
 
+
+def timestamp():
+    now = datetime.now()
+    year = str(now.year)
+    month = str(now.month)
+    day = str(now.day)
+    hour = str(now.hour)
+    minute = str(now.minute)
+    second = str(now.second)
+    return f"[{year:0>2}/{month:0>2}/{day:0>2}-{hour:0>2}:{minute:0>2}:{second:0>2}] "
 
 """----"""
 RED = "\033[1;31m"
@@ -50,34 +60,45 @@ def receive(client_socket):
     except:
         return False
 
-while True:
-    read_sockets, _, exception_sockets = select.select(SOCKET_LIST, [], SOCKET_LIST)
-    for socket in read_sockets:
-        if socket == server_socket:
-            client_socket, client_address = server_socket.accept()
+with open("message_log.txt", "a") as f:
+    f.write(timestamp() + "Created Server\n")
+    try:
+        while True:
+            read_sockets, _, exception_sockets = select.select(SOCKET_LIST, [], SOCKET_LIST)
+            for socket in read_sockets:
+                if socket == server_socket:
+                    client_socket, client_address = server_socket.accept()
 
-            user = receive(client_socket)
-            if not user:
-                continue
-            SOCKET_LIST.append(client_socket)
-            CLIENT_DICT[client_socket] = user
-            print(
-                f'{user["data"].decode()} Connected from ' + _PINK + f'{client_address[0]}:{client_address[1]}' + RESET)
-        else:
-            message = receive(socket)
-            if not message:
-                print(RED + f'Connection Closed From {CLIENT_DICT[socket]["data"].decode()}' + RESET)
+                    user = receive(client_socket)
+                    if not user:
+                        continue
+                    SOCKET_LIST.append(client_socket)
+                    CLIENT_DICT[client_socket] = user
+                    print(
+                        f'{user["data"].decode()} Connected from ' + _PINK + f'{client_address[0]}:{client_address[1]}'
+                        + RESET)
+                    f.write(timestamp() +
+                        f"{user['data'].decode()} Connected From {client_address[0]}:{client_address[1]}\n")
+                else:
+                    message = receive(socket)
+                    if not message:
+                        print(RED + f'Connection Closed From {CLIENT_DICT[socket]["data"].decode()}' + RESET)
+                        f.write(timestamp() + f"{CLIENT_DICT[socket]['data'].decode()} Disconnected\n")
+                        SOCKET_LIST.remove(socket)
+                        del CLIENT_DICT[socket]
+                        continue
+                    user = CLIENT_DICT[socket]
+                    print(f'Received From ' + GREEN + f'{user["data"].decode()}' +
+                          CYAN + f' "{message["data"].decode()}"' + RESET + f' at {datetime.now()}')
+                    f.write(timestamp() + f"{user['data'].decode()} : {message['data'].decode()}\n")
+
+                    for client_socket in CLIENT_DICT:
+                        if client_socket != socket:
+                            client_socket.send(user["header"] + user["data"] + message["header"] + message["data"])
+
+            for socket in exception_sockets:
                 SOCKET_LIST.remove(socket)
                 del CLIENT_DICT[socket]
-                continue
-            user = CLIENT_DICT[socket]
-            print(f'Received From ' + GREEN + f'{user["data"].decode()}' +
-                  CYAN + f' "{message["data"].decode()}"' + RESET + f' at {datetime.datetime.now()}')
-
-            for client_socket in CLIENT_DICT:
-                if client_socket != socket:
-                    client_socket.send(user["header"] + user["data"] + message["header"] + message["data"])
-
-    for socket in exception_sockets:
-        SOCKET_LIST.remove(socket)
-        del CLIENT_DICT[socket]
+    finally:
+        f.write(timestamp() + "Server Closed\n")
+        f.write("-" * 62 + '\n')
